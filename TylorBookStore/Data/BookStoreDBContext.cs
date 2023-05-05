e6e3c95e973895dadd9759f10dcb9a781b253a04
Tylor <46426868+DontDoThat21@users.noreply.github.com>
2023-05-05 13:10:31 -0400

    Added first EF migration to include Category table in new code first DB.

---
+++ TylorBookStore/Data/BookStoreDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TylorBookStore.Models;
+
+namespace TylorBookStore.Data
+{
+    public class BookStoreDBContext : DbContext
+    {
+        public BookStoreDBContext(DbContextOptions<BookStoreDBContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
