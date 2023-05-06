918d653671b0a214b480f5ca8c1ea25ebbbda61e
Tylor <46426868+DontDoThat21@users.noreply.github.com>
2023-05-06 19:07:56 -0400

    Added motorcycle connection and dependency injection rule.

--- TylorPortfolio/Program.cs
+++ TylorPortfolio/Program.cs
@@ -7,7 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<BookStoreDBContext>(options => 
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+    .UseSqlServer(builder.Configuration.GetConnectionString("MotorcycleConnection")));
 
 var app = builder.Build();
 
