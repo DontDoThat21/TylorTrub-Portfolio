0cb8892d08ade35e64b934724587ceede4ec26a1
Tylor <46426868+DontDoThat21@users.noreply.github.com>
2023-05-06 10:00:17 -0400

    Added edit IActionResults for categories.

--- TylorBookStore/Controllers/CategoryController.cs
+++ TylorBookStore/Controllers/CategoryController.cs
@@ -45,5 +45,37 @@ namespace TylorBookStore.Controllers
                 return View();
             }
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order cannot exactly match the name.");
+            }
+            if (obj.Name != null && obj.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("", "Test is an invalid value.");
+            }
+            if (ModelState.IsValid)
+            {
+                _bookstore.Categories.Add(obj);
+                _bookstore.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
     }
 }
