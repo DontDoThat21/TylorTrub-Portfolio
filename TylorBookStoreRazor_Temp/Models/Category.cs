6b3642d4c98fa06889f5b70a9070daccf15e8d26
Tylor <46426868+DontDoThat21@users.noreply.github.com>
2023-05-06 13:35:00 -0400

    Added EF Core Razor Page conn str. Added model to Razor solution.

---
+++ TylorBookStoreRazor_Temp/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace TylorBookStoreRazor_Temp.Models
+{
+    public class Category
+    {
+        [Key]
+        [DisplayName("Category Id")]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
+        [DefaultValue("")]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Display Order must be between 1-100.")]
+        public int DisplayOrder { get; set; }
+    }
+}
