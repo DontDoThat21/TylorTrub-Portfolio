6be37f82e186f27307e062d43182695f66b5f197
Tylor <46426868+DontDoThat21@users.noreply.github.com>
2023-05-07 11:48:02 -0400

    Added Repository and IRepository to begin following repository pattern for clean code. Added migrations with correct namespace and using statements.

---
+++ TylorPortfolio.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TylorTrubPortfolio.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        //T - Category
+        IEnumerable<T> GetAll();
+        T GetFirstOrDefault(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Delete(T entity);
+        void DeleteRange(IEnumerable<T> entity);
+
+        //T - Motorcycle
+    }
+}
---
+++ TylorPortfolio.DataAccess/Repository/Repository.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using TylorTrubPortfolio.DataAccess.Data;
+using TylorTrubPortfolio.DataAccess.Repository.IRepository;
+
+namespace TylorTrubPortfolio.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly BookStoreDBContext _bookStoreDBContext;
+        internal DbSet<T> dbSet;
+        private readonly MotorcycleDBContext _motorcycleDBContext;
+
+        /// <summary>
+        /// The motorcycle and bookstore DBs are seperated intentionally.
+        /// I don't see why they would logically be connected. Also, I like the challenge of
+        /// juggling two context and seeing how .NET Core 8 handles it. Seems good!
+        /// </summary>
+        /// <param name="book">BookStore and categories DB.</param>
+        /// <param name="motorcycle">Motorcycle store, information, and CDN DB.</param>        
+        public Repository(DbContext _db)
+        {
+            try
+            {
+                _bookStoreDBContext = (BookStoreDBContext?)_db;
+                this.dbSet = _db.Set<T>();
+            }
+            catch (Exception)
+            {
+            }
+            try
+            {
+                _motorcycleDBContext = (MotorcycleDBContext?)_db;
+                this.dbSet = _db.Set<T>();
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }        
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = dbSet;
+            query = query.Where(filter);
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = dbSet;
+            return query.ToList();
+        }
+
+        public void Delete(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void DeleteRange(IEnumerable<T> entity)
+        {
+            dbSet.RemoveRange(entity);
+        }
+    }
+}
