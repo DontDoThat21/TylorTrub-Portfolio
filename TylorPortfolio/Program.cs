c8516675fb202f897fc5d69b941feb3d85a03422
Tylor <46426868+DontDoThat21@users.noreply.github.com>
2023-05-06 20:48:58 -0400

    Revert "Added motorcycle connection and dependency injection rule."
    This reverts commit 918d653671b0a214b480f5ca8c1ea25ebbbda61e.

--- TylorPortfolio/Program.cs
+++ TylorPortfolio/Program.cs
@@ -7,8 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<BookStoreDBContext>(options => 
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
-    .UseSqlServer(builder.Configuration.GetConnectionString("MotorcycleConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
