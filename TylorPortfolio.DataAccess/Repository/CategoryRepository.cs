5010d8de5da06fb500380b3c3c2d4fd419a98a95
Tylor <46426868+DontDoThat21@users.noreply.github.com>
2023-05-07 11:59:11 -0400

    Added Category interfance and repository w/ repo pattern.

---
+++ TylorPortfolio.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using TylorTrubPortfolio.DataAccess.Data;
+using TylorTrubPortfolio.DataAccess.Repository.IRepository;
+using TylorTrubPortfolio.Models;
+
+namespace TylorTrubPortfolio.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private BookStoreDBContext _db;
+
+        public CategoryRepository(BookStoreDBContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
+    }
+}
---
+++ TylorPortfolio.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using TylorTrubPortfolio.Models;
+
+namespace TylorTrubPortfolio.DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category obj);
+        void Save();
+    }
+}
