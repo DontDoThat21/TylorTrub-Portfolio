daf165aa4f32e2fdeabc1e21b33dcbe06def8223
Tylor <46426868+DontDoThat21@users.noreply.github.com>
2023-05-08 19:57:24 -0400

    Added motorcycle repos.

---
+++ TylorPortfolio.DataAccess/Repository/MotorcycleRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TylorTrubPortfolio.DataAccess.Data;
+using TylorTrubPortfolio.DataAccess.Repository.IRepository;
+using TylorTrubPortfolio.Models;
+
+namespace TylorTrubPortfolio.DataAccess.Repository
+{
+    public class MotorcycleRepository : Repository<Motorcycle>, IMotorcycleRepository
+    {
+        private MotorcycleDBContext _db;
+
+        public MotorcycleRepository(MotorcycleDBContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Motorcycle obj)
+        {
+            _db.Motorcycles.Update(obj);
+        }
+    }
+}
