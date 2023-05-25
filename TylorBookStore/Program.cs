f5eae19721d420925d26b2b1b17533522500bdd9
Tylor <46426868+DontDoThat21@users.noreply.github.com>
2023-05-25 18:49:00 -0400

    Fixed app cookie login redirects.

--- TylorBookStore/Program.cs
+++ TylorBookStore/Program.cs
@@ -16,7 +16,15 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<PortfolioDBContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+
 builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<PortfolioDBContext>().AddDefaultTokenProviders();
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = $"/Identity/Account/Login";
+    options.LogoutPath = $"/Identity/Account/Logout";
+    options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
+});
+
 builder.Services.AddRazorPages();
 //builder.Services.AddDbContext<MotorcycleDBContext>(options => 
 //    options.UseSqlServer(builder.Configuration.GetConnectionString("MotorcycleConnection")));
