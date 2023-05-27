002d4735d759c8137760fd0267f8400dbf66917f
Tylor <46426868+DontDoThat21@users.noreply.github.com>
2023-05-27 19:23:09 -0400

    Fixed incorrect app breaking comment in cat. controller.

--- TylorPortfolio/Areas/Admin/Controllers/CategoryController.cs
+++ TylorPortfolio/Areas/Admin/Controllers/CategoryController.cs
@@ -9,8 +9,8 @@ using TylorTrubPortfolio.Utility;
 
 namespace TylorTrubPortfolio.Areas.Admin.Controllers
 {
-    //[Area("Admin")]
-    [Authorize(Roles = SD.Role_Admin)]
+    [Area("Admin")]
+    //[Authorize(Roles = SD.Role_Admin)]
     public class CategoryController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
