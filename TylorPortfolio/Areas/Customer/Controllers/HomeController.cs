40ef3a3a08b30a3d283cbf93bd75687a250aec1c
Tylor <46426868+DontDoThat21@users.noreply.github.com>
2023-05-27 22:16:13 -0400

    Added required special identity session clause. Added dependency injection for home controller. Added details post method body.

--- TylorPortfolio/Areas/Customer/Controllers/HomeController.cs
+++ TylorPortfolio/Areas/Customer/Controllers/HomeController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using System.Security.Claims;
+using TylorTrubPortfolio.DataAccess.Data;
 using TylorTrubPortfolio.DataAccess.Repository.IRepository;
 using TylorTrubPortfolio.Models;
 using TylorTrubPortfolio.Models.ViewModels;
@@ -9,13 +13,16 @@ namespace TylorTrubPortfolio.Areas.Customer.Controllers
     [Area("Customer")]
     public class HomeController : Controller
     {
+        private PortfolioDBContext _context;// = new PortfolioDBContext();
+
         private readonly ILogger<HomeController> _logger;
         private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork, PortfolioDBContext context)
         {
             _logger = logger;
             _unitOfWork = unitOfWork;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -34,15 +41,55 @@ namespace TylorTrubPortfolio.Areas.Customer.Controllers
             return View(productList);
         }
 
-        public IActionResult Details(int productId)
+        public IActionResult Details(int id)
         {
             ShoppingCart cart = new()
             {
-                Product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category"),
+                Product = _unitOfWork.Product.Get(u => u.Id == id, includeProperties: "Category"),
                 Count = 1,
-                ProductId = productId
+                ProductId = id
             };
             return View(cart);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult Details(ShoppingCart shoppingCart)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+            shoppingCart.ApplicationUserId = userId;
+
+            ShoppingCart cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.ApplicationUserId == userId &&
+            u.ProductId == shoppingCart.ProductId);
+
+            _context.Database.OpenConnection();
+            _context.Database.ExecuteSqlRaw("SET IDENTITY_INSERT dbo.ShoppingCarts ON");
+            _context.SaveChanges();            
+
+            if (cartFromDb != null)
+            {
+                //shopping cart exists
+                cartFromDb.Count += shoppingCart.Count;
+                _unitOfWork.ShoppingCart.Update(cartFromDb);
+                _unitOfWork.Save();
+            }
+            else
+            {
+                //add cart record
+                _unitOfWork.ShoppingCart.Add(shoppingCart);
+                _unitOfWork.Save();
+                //HttpContext.Session.SetInt32(SD.SessionCart,
+                //_unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId).Count());
+            }
+            _context.Database.ExecuteSqlRaw("SET IDENTITY_INSERT dbo.ShoppingCarts OFF");
+            _context.Database.CloseConnection();
+            TempData["success"] = "Cart updated successfully";
+
+
+
+
+            return RedirectToAction(nameof(MVCDemo));
         }
 
         public IActionResult Privacy()
